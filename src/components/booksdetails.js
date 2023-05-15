5c0eb38e7d158d9445692bc75b31957379790038
mohitjha123 <mohit.jha@bazaarnxt.com>
2023-05-15 16:35:22 +0530

    Stored all the details in variable

--- src/components/booksdetails.js
+++ src/components/booksdetails.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+const img ='./img/91bYsX41DVL._AC_UL600_SR600,400_.jpg';
+const title = "Atomic Habits: the life-changing million-copy #1 bestseller";
+const author = "James Clear";
+
 const Book = () =>{
-    const title = "Atomic Habits: the life-changing million-copy #1 bestseller";
-    const author = "James Clear";
     return <article className='book'>
-      <img src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg" alt="Atomic Habits" />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h3>{author.toUpperCase()}</h3>
     </article>
