91812765547ad35f474a1586aca0c2c5aef75c06
mohitjha123 <mohit.jha@bazaarnxt.com>
2023-05-16 14:35:34 +0530

    deleted components folder

--- src/index.js
+++ src/index.js
@@ -1,41 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Book from './components/booksdetails';
 
-const firstBook = {
-  author: 'James Clear',
-  img: 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg',
-  title: 'Atomic Habits: the life-changing million-copy #1 bestseller',
-};
-const secBook = {
-  author: ' Anthony Doerr',
-  img: './img/1501173219.01.S001.LXXXXXXX.jpg',
-  title: 'All the Light we Cannot See',
-};
-const thirdBook = {
-  author: ' Morgan Housel',
-  img: 'https://m.media-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
-  title: 'The Psychology of Money',
-};
-
+const img ='./img/91bYsX41DVL._AC_UL600_SR600,400_.jpg';
+const title = "Atomic Habits: the life-changing million-copy #1 bestseller";
+const author = "James Clear";
 
 const BookList = () => {
   return <section className='booklist'>
-    <Book
-      author={firstBook.author}
-      title={firstBook.title}
-      img={firstBook.img} />
-    <Book
-      author={secBook.author}
-      title={secBook.title}
-      img={secBook.img} />
-    <Book
-      author={thirdBook.author}
-      title={thirdBook.title}
-      img={thirdBook.img} />
+    <Book author={author} title={title} img={img} />
+    <Book author={author} title={title} img={img} />
+    <Book author={author} title={title} img={img} />
   </section>
 }
-
+const Book = (props) =>{
+  console.log(props)
+  return <article className='book'>
+    <img src={props.img} alt={props.title} />
+    <h2>{props.title}</h2>
+    <h3>{props.author}</h3>
+  </article>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList></BookList>);
