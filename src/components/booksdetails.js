92c6e93c3df8251d1615399472f75fbcaafac738
mohitjha123 <mohit.jha@bazaarnxt.com>
2023-05-16 09:36:10 +0530

    added props

--- src/components/booksdetails.js
+++ src/components/booksdetails.js
@@ -1,16 +1,12 @@
 import React from 'react'
 
-const img ='./img/91bYsX41DVL._AC_UL600_SR600,400_.jpg';
-const title = "Atomic Habits: the life-changing million-copy #1 bestseller";
-const author = "James Clear";
 
-const Book = () =>{
+const Book = ({img, title, author}) =>{
     return <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <h3>{author.toUpperCase()}</h3>
+      <h3>{author}</h3>
     </article>
   }
-
 
 export default Book
